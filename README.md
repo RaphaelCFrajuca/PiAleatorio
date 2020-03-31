19047ab21b62feab99c066d259db9da1680cb7b8
Raphael Frajuca <raphaelcfrajuca@gmail.com>
2020-03-31 01:38:12 -0300

    Mudança no nome da função e melhora na precisão do gerador de números aleatórios

--- README.md
+++ README.md
@@ -4,10 +4,6 @@ Programa simples feito em Python para obter números aleatórios com o calculo d
 ## Como usar:
 
 ```python
-from Pi-Aleatorio import pi
-pi()
+from PiAleatorio import pi_aleatorio
+pi_aleatoio.randomint()
 ```
-
-## Notas da versão:
-
-#### 1.0 - Adicionada função simples de gerar somente 1 número de 0 a 9 por vez.
--- pi_aleatorio.py
+++ pi_aleatorio.py
@@ -8,7 +8,7 @@ from decimal import getcontext
 # Importando módulo de datas e tempo
 from time import time
 
-def pi():
+def randomint():
     
     # Pegar milissegundos atuais
     milisegundos = time()
@@ -16,11 +16,11 @@ def pi():
     # Calcular o valor de Pi usando valores padronizados. (Circunferência / Raio)
 
     # Pegar ultimos números do valor de milissegundos atuais se baseando no relógio da maquina.
-    mili = int(repr(milisegundos/10000000)[12:16])
-
+    mili = int(str(int(round(time() * 1000)))[-5:-1])
+    
     # Calcular valor de Pi com o número de casas decimais de acordo com o valor retornado pelos milissegundos.
     getcontext().prec = mili
-    pi = str(Decimal(355000000) / Decimal(113000000))
+    pi = str(Decimal(3550000000000000000000000000000000000000000000000000000000) / Decimal(1130000000000000000000000000000000000000000000000000000000))
 
     # Pegar o ultimo decimal do Pi e retornar 
     return(int(pi[-1]))
