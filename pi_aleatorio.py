c75261d0ff4aebe026de0ebac2e0c4eb71fd7010
Raphael Frajuca <raphaelcfrajuca@gmail.com>
2020-04-04 08:41:16 -0300

    Melhora na precisão do gerador
    Foi efetuada uma melhora na precsão do gerador usando outra escala de tempo baseada em outra função do módulo time do Python, além de uma adição de licensa.

--- pi_aleatorio.py
+++ pi_aleatorio.py
@@ -5,22 +5,33 @@ __author__ = "Raphael Frajuca"
 from decimal import Decimal
 from decimal import getcontext
 
-# Importando módulo de datas e tempo
-from time import time
+# Importando módulo de  tempo
+import time
 
-def randomint():
+def randomint(numero1, numero2 = None):
     
-    # Pegar milissegundos atuais
-    milisegundos = time()
-
-    # Calcular o valor de Pi usando valores padronizados. (Circunferência / Raio)
-
-    # Pegar ultimos números do valor de milissegundos atuais se baseando no relógio da maquina.
-    mili = int(str(int(round(time() * 1000)))[-5:-1])
+    tempo = int(str(time.perf_counter()).split('.')[1])
     
-    # Calcular valor de Pi com o número de casas decimais de acordo com o valor retornado pelos milissegundos.
-    getcontext().prec = mili
-    pi = str(Decimal(3550000000000000000000000000000000000000000000000000000000) / Decimal(1130000000000000000000000000000000000000000000000000000000))
-
-    # Pegar o ultimo decimal do Pi e retornar 
-    return(int(pi[-1]))
+    if(numero1 != None & numero2 == None):
+        getcontext().prec = tempo
+        pi = str(Decimal(3550000000000000000000000000000000000000000000000000000000) / Decimal(1130000000000000000000000000000000000000000000000000000000))
+        limite = (numero1-1)
+        
+        limite_caracteres = int("-"+str(len(str(limite))))-1
+        
+        pi_aleatorio = int(pi[limite_caracteres:-1])
+        while(pi_aleatorio >= numero1):
+            tempo = int(str(time.perf_counter()).split('.')[1]) 
+            getcontext().prec = tempo
+            pi = str(Decimal(3550000000000000000000000000000000000000000000000000000000) / Decimal(1130000000000000000000000000000000000000000000000000000000))
+            limite = (numero1-1)
+        
+            limite_caracteres = int("-"+str(len(str(limite))))-1
+        
+            pi_aleatorio = int(pi[limite_caracteres:-1])
+        
+        
+        return(pi_aleatorio)
+        
+        
+            
